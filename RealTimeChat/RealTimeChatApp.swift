40885820af48efb1fa0919ed5f0e0fd870238b09
justinwongeecs <jwonghelloworld@berkeley.edu>
2025-04-10 17:23:50 -0700

    Initial Commit

---
+++ RealTimeChat/RealTimeChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimeChatApp.swift
+//  RealTimeChat
+//
+//  Created by Justin Wong on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RealTimeChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
